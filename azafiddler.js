fd4666a29612af06e15c2e5bbc63f703bcb99740
Peter Grape <peter.grape@gmail.com>
2020-06-15 17:22:31 +0200

    Update azafiddler.js

--- azafiddler.js
+++ azafiddler.js
@@ -3,7 +3,7 @@
 // @description		Some Avanza site tweaks to ease the trading day
 // @author			Anonymous
 // @include			https://www.avanza.se/*
-// @version 0.4
+// @version 0.5
 // @namespace https://greasyfork.org/users/593500
 // ==/UserScript==
 
@@ -11,12 +11,29 @@
 window.addEventListener("load", init, false);
 
 // Adds a simple external tweak to the Avanza links.
-////
+// Minor fixes for order window
 function init()
 {
   console.info("Hi Avanza!");
   // Tweak all links to external target.
   tweaklinks(document);
+  fixordervindow(document);
+}
+
+
+function fixordervindow(doc)
+{
+    if (window.name === "orderWindow") {
+        console.info("Avanza Orderwindow");
+        window.resizeTo(858,774);
+        let azaheader=doc.getElementsByTagName("aza-header")[0];
+        let azamenu=doc.getElementsByTagName("aza-menu")[0];
+        let azapmenu=doc.getElementsByTagName("aza-personal-menu")[0];
+
+        azaheader.style.display="none";
+        azamenu.style.display="none";
+        azapmenu.style.display="none";
+    }
 }
 
 function tweaklinks(doc)
